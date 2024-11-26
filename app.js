2dfe0f63348cef588ef7994476bad0e6fd3fe1ed
nam pham <contact.nampham.work@gmail.com>
2024-11-26 14:17:50 +0700

    feat: post request

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import createHomePageTeamplate from "./views/index.js";
+import BOOKS_DATA from "./data/data.js";
 import CreateListTeamplate from "./views/list.js";
 
 const app = express();
@@ -14,6 +15,14 @@ app.get("/", (req, res) => {
 app.get('/books', (req, res) => {
     res.send(CreateListTeamplate());
 })
+
+app.post('/books', (req, res) => {
+   const { title, author }  = req.body;
+   const id = Math.random().toString();
+   BOOKS_DATA.push({ id, title, author });
+
+   res.send(`<li>${title}, ${author}</li>`);
+});
 
 app.listen(3000, () => {
     console.log("App listening on port 3000");
--- views/index.js
+++ views/index.js
@@ -14,15 +14,30 @@ const createHomepageTemplate = () => /*html*/`
       <main>
         <div class="book-list">
           <!-- book list here later -->
-           <!-- <button hx-get="/books" hx-target=".book-list">Show books</button> -->
+           <button hx-get="/books" hx-target=".book-list">Show books</button>
            <!-- <button hx-get="/books" hx-swap="beforeend", hx-target=".book-list"></button> -->
            <!-- <button hx-get="/books" hx-swap="afterend", hx-target=".book-list"></button> -->
            <!-- <button hx-get="/books" hx-swap="outerHTML", hx-target=".book-list"></button> -->
-            <button hx-get="/books" hx-swap="innerHTML", hx-target=".book-list"></button>
+            <!-- <button hx-get="/books" hx-swap="innerHTML", hx-target=".book-list"></button> -->
         </div>
 
         <div class="add-book-form">
           <h2>What do you want to read?</h2>
+          <form>
+            <input 
+              id = "title"
+              type="text"
+              name="title"
+              placeholder="title"
+            />
+            <input 
+              id = "author"
+              type="text"
+              name="author"
+              placeholder="author"
+            />
+            <button hx-post="./books" hx-target=".book-list ul" hx-swap="beforeend">Add</button>
+          </form>
           <!-- form template here later -->
         </div>
       </main>
