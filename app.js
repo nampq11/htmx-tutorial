9d41aeb1223c20a1c5991d41d8f16cd4c18562fc
nam pham <contact.nampham.work@gmail.com>
2024-11-26 14:23:24 +0700

    feat: add redirects

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import createHomePageTeamplate from "./views/index.js";
 import BOOKS_DATA from "./data/data.js";
 import CreateListTeamplate from "./views/list.js";
+import createBookTeamplate from "./views/book.js";
 
 const app = express();
 app.use(express.urlencoded({extended: false}));
@@ -21,7 +22,14 @@ app.post('/books', (req, res) => {
    const id = Math.random().toString();
    BOOKS_DATA.push({ id, title, author });
 
-   res.send(`<li>${title}, ${author}</li>`);
+   res.redirect('/books/' + id);
+});
+
+app.get('/books/:id', (req, res) => {
+    const { id } = req.params;
+    const book = BOOKS_DATA.find(book => book.id === id);
+
+    res.send(createBookTeamplate(book));
 });
 
 app.listen(3000, () => {
